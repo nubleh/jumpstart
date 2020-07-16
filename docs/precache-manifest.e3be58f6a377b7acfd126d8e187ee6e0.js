3385f0170bc5366d19953ba47717ed30f77f6b0e
Nubli Naim <nubli.naim@gmail.com>
2020-07-16 21:19:47 +0900

    chore: build into docs

---
+++ docs/precache-manifest.e3be58f6a377b7acfd126d8e187ee6e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4c461f31f30ff5a6a6b4973a6209f97",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7d4f932d0f60a1c1d769",
+    "url": "/static/js/2.0925d135.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.0925d135.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "860a0d95aece787e558c",
+    "url": "/static/js/main.356b187e.chunk.js"
+  },
+  {
+    "revision": "432a01943a7d07079585",
+    "url": "/static/js/runtime-main.d48f2669.js"
+  },
+  {
+    "revision": "a0c9a3d3315d7c87b521c0c98cc97b6e",
+    "url": "/static/media/506px-Jumpstart_Booster.a0c9a3d3.png"
+  }
+]);
